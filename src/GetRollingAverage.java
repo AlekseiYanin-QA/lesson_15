492c2e748056a04b2bf0163f05cf776307c26c35
Yanin.A.V <aleksei.yanin.qa@gmail.com>
2024-10-29 12:08:43 +0300

    доработка Exception на k

--- src/GetRollingAverage.java
+++ src/GetRollingAverage.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 public class GetRollingAverage {
     public static List<Double> getRollingAverage(List<Integer> arr, int k) {
+        int n = arr.size();
+        // Проверка на допустимость значения k
+        if (k <= 0 || k > n) {
+            throw new IllegalArgumentException("k должно быть положительным и меньше или равно размеру массива.");
+        }
         List<Double> result = new ArrayList<>();
         double windowSum = 0.0;
 
